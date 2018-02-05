4fadcb11d66cb1212aaf48a34e778a9c36bc49a0
a029210979j <a029210979j@S09-PC16>
2018-02-05 09:42:38 +0100

    foodies-client - added carrito profile 3 -wip

---
+++ public_html/js/app/carrito/3/remove.js
@@ -0,0 +1,39 @@
+'use strict';
+
+moduloCarrito.controller('CarritoRemove3Controller',
+        ['$scope', '$routeParams', 'serverCallService', '$location', 'sessionService', 'constantService','objectService',
+            function ($scope, $routeParams, serverCallService, $location, sessionService, constantService,objectService) {
+                $scope.ob = "carrito";
+                $scope.op = "remove";
+                $scope.profile = 3;
+                //---
+                $scope.status = null;
+                $scope.debugging = constantService.debugging();
+                $scope.url = $scope.ob + '/' + $scope.profile + '/' + $scope.op;
+                //---
+                $scope.id_producto = $routeParams.id_producto;
+                //---
+                $scope.objectService = objectService;
+                //---
+                $scope.remove = function () {
+                    serverCallService.remove($scope.ob, $scope.id_producto).then(function (response) {
+                        if (response.status == 200) {
+                            if (response.data.status == 200) {                                
+                                    $scope.status = "El registro con id=" + $scope.id_producto + " se ha eliminado.";
+                                } else {
+                                    $scope.status = "Error en el borrado de datos del servidor";
+                                }
+                            } else {
+                                $scope.status = "Error en la recepción de datos del servidor";
+                            }                        
+                    }).catch(function (data) {
+                        $scope.status = "Error en la recepción de datos del servidor";
+                    });
+                }
+                $scope.back = function () {
+                    window.history.back();
+                };
+                $scope.close = function () {
+                    $location.path('/home');
+                };
+            }]);

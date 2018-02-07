db3b225d9768a0802f1d72d2ac1711b322e67a71
a029210979j <a029210979j@S09-PC16>
2018-02-07 10:38:05 +0100

    foodies-client - cart plist shows nothing  -wip

--- public_html/js/app/carrito/1/plist.js
+++ public_html/js/app/carrito/1/plist.js
@@ -1,62 +1,41 @@
 'use strict';
 moduloCarrito.controller('CarritoPlist1Controller',
-  ['$scope', '$routeParams', '$location', 'serverCallService', 'toolService', 'constantService',
-            function ($scope, $routeParams, $location, serverCallService, toolService, constantService) {        $scope.ob = "carrito";
+        ['$scope', '$routeParams', 'serverCallService', '$location', 'sessionService', 'constantService',
+            function ($scope, $routeParams, serverCallService, $location, sessionService, constantService) {
+                $scope.ob = "carrito";
                 $scope.op = "plist";
                 $scope.profile = 1;
                 //---
+                $scope.id = $routeParams.id;
                 //---
                 $scope.url = $scope.ob + '/' + $scope.profile + '/' + $scope.op;
-                //----
-                $scope.numpage = toolService.checkDefault(1, $routeParams.page);
-                $scope.rpp = toolService.checkDefault(10, $routeParams.rpp);
-                $scope.neighbourhood = constantService.getGlobalNeighbourhood();
-                //---
-                $scope.orderParams = toolService.checkEmptyString($routeParams.order);
-                $scope.filterParams = toolService.checkEmptyString($routeParams.filter);
                 //---
                 $scope.status = null;
                 $scope.debugging = constantService.debugging();
                 //---
-                function getDataFromServer() {
-                    serverCallService.getCount($scope.ob, $scope.filterParams).then(function (response) {
-                        if (response.status == 200) {
-                            $scope.registers = response.data.json;
-                            $scope.pages = toolService.calculatePages($scope.rpp, $scope.registers);
-                            if ($scope.numpage > $scope.pages) {
-                                $scope.numpage = $scope.pages;
-                            }
-                            return serverCallService.getPage($scope.ob, $scope.rpp, $scope.numpage, $scope.filterParams, $routeParams.order);
-                        } else {
-                            $scope.status = "Error en la recepción de datos del servidor";
-                        }
-                    }).then(function (response) {
-                        if (response.status == 200) {
-                            $scope.page = response.data.json.data;
+                serverCallService.list($scope.ob).then(function (response) {
+                    if (response.status == 200) {
+                        if (response.data.status == 200) {
+                            $scope.status = null;
+                            $scope.bean = response.data.json.data;
                             $scope.metao = response.data.json.metaObject;
                             $scope.metap = response.data.json.metaProperties;
+
+
                         } else {
                             $scope.status = "Error en la recepción de datos del servidor";
                         }
-                    }).catch(function (data) {
+                    } else {
                         $scope.status = "Error en la recepción de datos del servidor";
-                    });
-                }
-                $scope.doorder = function (orderField, ascDesc) {
-                    $location.url($scope.url + '/' + $scope.numpage + '/' + $scope.rpp).search('filter', $scope.filterParams).search('order', orderField + ',' + ascDesc);
-                    return false;
-                };
+                    }
+                }).catch(function (data) {
+                    $scope.status = "Error en la recepción de datos del servidor";
+                });
                 $scope.back = function () {
                     window.history.back();
                 };
                 $scope.close = function () {
                     $location.path('/home');
                 };
-                $scope.setShowRemove = function (show) {
-                    $scope.showRemove = show;
-                };
-                getDataFromServer();
             }
         ]);
-
-
